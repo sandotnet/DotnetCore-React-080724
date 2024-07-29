84048fd71ef88eed54db95984dd417410ed47131
sandotnet <87011674+sandotnet@users.noreply.github.com>
2024-07-29 11:53:58 +0530

    commit_2

---
+++ MVC/HandsOn/MyFirstMVCAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstMVCAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
