ef73fa2e036a3dfa93bec341c30a7183bdc991ea
sandotnet <87011674+sandotnet@users.noreply.github.com>
2024-08-08 15:00:35 +0530

    commit_2

--- API/HandsOn/HandsOnAPIUsingEFCodeFirst/Program.cs
+++ API/HandsOn/HandsOnAPIUsingEFCodeFirst/Program.cs
@@ -3,6 +3,7 @@ using HandsOnAPIUsingEFCodeFirst.Entities;
 using HandsOnAPIUsingEFCodeFirst.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace HandsOnAPIUsingEFCodeFirst
@@ -20,24 +21,58 @@ namespace HandsOnAPIUsingEFCodeFirst
             builder.Services.AddTransient<IUserRepository, UserRepository>();
             builder.Services.AddControllers();
             //Configure JWT to valie token data
+            #region TokenValidationCode
             builder.Services.AddAuthentication(options =>
+               {
+                   options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                   options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                   options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+               }).AddJwtBearer(o =>
+               {
+                   o.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                       ValidAudience = builder.Configuration["Jwt:Audience"],
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = false,
+                       ValidateIssuerSigningKey = true
+                   };
+               });
+            #endregion
+            //Configure Swagger UI to validate token
+            #region Swagger UI for Validate Token
+            builder.Services.AddSwaggerGen(c =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(o =>
-            {
-                o.TokenValidationParameters = new TokenValidationParameters
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = false,
-                    ValidateIssuerSigningKey = true
-                };
-            });
+                    Title = "JWTToken_Auth_API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+            }); 
+            #endregion
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
