26b08650126201dd3ddf994408390cd01386752e
NagaLakshmio701 <nagalakshmisingamsetty9390@gmail.com>
2024-08-28 11:32:14 +0530

    s

--- React/HandsOn/react-route-demo1/src/App.js
+++ React/HandsOn/react-route-demo1/src/App.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { BrowserRouter,Routes ,Route} from "react-router-dom";
 import logo from "./logo.svg";
+import  Home from "./Pages/home";
+import  Blogs from "./Pages/blogs";
+import  Contact from "./Pages/contact";
+import  NoPage from "./Pages/nopage";
+import  Layout from "./Pages/layout";
+
+
+
+
 import "./App.css";
 function App() {
-  return <h1></h1>;
+  return (
+  <>
+  <BrowserRouter>
+  <Routes>
+        <Route path ="/" element={<Layout/>}>
+
+        <Route index element={<Home/>}/>
+        <Route path="contact" element={<Contact/>}/>
+
+        <Route path="blogs" element={<Blogs/>}/>
+        </Route>
+        <Route path="*" element={<NoPage/>}/>
+
+
+
+  </Routes>
+  
+  </BrowserRouter>
+  
+  
+  </>)
 }
 
 export default App;
--- React/HandsOn/react-route-demo1/src/Pages/contact.js
+++ React/HandsOn/react-route-demo1/src/Pages/contact.js
@@ -1,6 +1,8 @@
 import React from "react";
-const Contact = () => {
+const Contact = () =>{
+  <div className="mine">
   return <h1>Contact Me</h1>;
+  </div>
 };
 
 export default Contact;
